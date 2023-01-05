12429d565fbfca976acd9ebda0c11471bfa8c1ce
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2023-01-05 20:13:58 +0900

    認証用関数叩き台完成

--- backend/aws_kms_lambda_ethereum/_lambda/functions/auth_client/lambda_function.py
+++ backend/aws_kms_lambda_ethereum/_lambda/functions/auth_client/lambda_function.py
@@ -4,7 +4,7 @@ import logging
 import os
 import sys
 from web3 import Web3, HTTPProvider
-#from lambda_helper import (verify_signature)
+from eth_account.messages import encode_defunct
 
 LOG_LEVEL = os.getenv("LOG_LEVEL", "WARNING")
 LOG_FORMAT = "%(levelname)s:%(lineno)s:%(message)s"
@@ -22,7 +22,8 @@ handler.setFormatter(formatter)
 
 '''
 クライアントから受け取ったデジタル署名について、
-アドレス(≒公開鍵)と元のメッセージを使って検証を行う
+アドレス(≒公開鍵)と元のメッセージを使って検証する
+問題なければ一時認証トークンを返す
 '''
 def lambda_handler(event, context):
     _logger.debug("incoming event: {}".format(event))
@@ -38,11 +39,14 @@ def lambda_handler(event, context):
     message = event.get('message')
     signature = event.get('signature')
 
-    actual_addr = w3.eth.accounts.recover(message, signature)
+    message_hash = encode_defunct(text=message)
+    #actual_addr = w3.eth.account.recoverHash(message_hash, signature=signature)
+    actual_addr = w3.eth.account.recover_message(message_hash, signature=signature)
     isVerified = actual_addr == tgt_addr
 
     if isVerified:
         return {
+            'address': actual_addr,
             'token': 'eyJ0e...'
         }
     else:
